89f938b0a74356fe8b118ff17f19562415a7efe9
Alexandre Chagas <afschagas@gmail.com>
2023-03-28 10:36:04 -0300

    New Project back-end

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3001;
+
+// para reconhecer os dados recebidos como sendo um objeto no formato JSON
+app.use(express.json());
+
+// Arquivo com rotas para o cadastro de livros
+const livros = require("./livros");
+
+//Identificação da rota e da const (require) associada
+app.use("/livros", livros);
+
+app.get("/", (req, res) => {
+  res.send("Olá... Bem vindo!");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
---
+++ data/seeds/001_add_livros.js
@@ -0,0 +1,42 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+// exports.seed = async function(knex) {
+//   // Deletes ALL existing entries
+//   await knex('table_name').del()
+//   await knex('table_name').insert([
+//     {id: 1, colName: 'rowValue1'},
+//     {id: 2, colName: 'rowValue2'},
+//     {id: 3, colName: 'rowValue3'}
+//   ]);
+// };
+
+
+exports.seed = function (knex) {
+  return knex("livros").del()
+  .then(function () {
+    return knex("livros").insert([
+      {
+        titulo: "Web Design Responsio", autor: "Maurício Samy Silva", ano: 2014,
+        preco: 73.0, foto: "https://s3.novatec.com.br/capas/9788575223925.jpg",
+      },
+      {
+        titulo: "Proteção Moderna de Dados", autor: "W. Curtis Preston", ano: 2021,
+        preco: 97.0, foto: "https://s3.novatec.com.br/capas/9786586057843.jpg",
+      },
+      {
+        titulo: "SQL em 10 Minutos por Dia", autor: "Bem Forta", ano: 2021,
+        preco: 79.0, foto: "https://s3.novatec.com.br/capas/97865860578447.jpg",
+      },
+      {
+        titulo: "CSS Grid Layout", autor: "Maurício Samy Silva", ano: 2017,
+        preco: 45.0, foto: "https://s3.novatec.com.br/capas/9788575226322.jpg",
+      },
+      {
+        titulo: "Python para análise de dados", autor: "Wes McKinney", ano: 2018,
+        preco: 132.0, foto: "https://s3.novatec.com.br/capas/9788575226476.jpg",
+      },
+    ]);
+  });
+}
