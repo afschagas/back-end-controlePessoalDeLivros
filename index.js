caaf05305828d8c941ecaae34dbb2659ffc3ca1e
Alexandre Chagas <afschagas@gmail.com>
2023-04-09 19:52:56 -0300

    Configuração da porta

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = 3001;
+const port = 3000;
 
 // para reconhecer os dados recebidos como sendo um objeto no formato JSON
 app.use(express.json());
